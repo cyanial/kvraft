ea5c96677ed57213b50a4bb1321e14d3a8f23776
cyanial <chenjie3323@sjtu.edu.cn>
2022-06-02 16:52:32 +0800

    init repo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cyanial/kvraft
+
+go 1.18
+
+require github.com/cyanial/raft v0.0.0-20220602074712-0dd95581fc11
---
+++ util.go
@@ -0,0 +1,16 @@
+package kvraft
+
+import "log"
+
+const Debug = false
+
+func init() {
+	log.SetFlags(log.Ltime)
+}
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
